6492e843b2d988d18b92dc430a5320efbe9977cf
Mariana Licona <m.licona@abtis.de>
2021-06-13 15:39:01 +0200

    create  exceptions block for COLORaSIGNATION class

--- COLORaSIGNATION.cs
+++ COLORaSIGNATION.cs
@@ -1,10 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace TideUpPhotografies
 {
-    class COLORaSIGNATION
+    class COLORaSIGNATION:PHOTO
     {
+     public COLORaSIGNATION(int row, int column):base(row, column)
+        {
+        }
+        public override int storePhoto()
+        {
+            //Bloque de atrapado de excepcion IONot Implemented
+            try
+            {
+                ShowMatrixInputs();
+            }
+            catch(IOException ioe)
+            {
+                throw new NotImplementedException();
+                Console.WriteLine(ioe);
+            }
+
+            return base.column;
+        }
     }
 }

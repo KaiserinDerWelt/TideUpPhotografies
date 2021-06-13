460ead00d8bfa25755ff50c1a2710a109ddb98ca
Mariana Licona <m.licona@abtis.de>
2021-06-13 17:00:14 +0200

    first functional version

--- Program.cs
+++ Program.cs
@@ -5,7 +5,9 @@ namespace TideUpPhotografies
     class Program
     {
         static void Main(string[] args)
-        {   //Marco de bienvenida
+        {
+            string divider = "<------>";
+            //Marco de bienvenida
             Console.WriteLine("<-------------------------------------->");
             Console.WriteLine("<------Bienvenido a Photo Tide Up------>");
             Console.WriteLine("Sigue las instrucciones para ordenar tus fotos");
@@ -30,7 +32,7 @@ namespace TideUpPhotografies
                     //Lectura de columnas por loopeo
                     for(int columnas = 0; columnas<bidimensionalArray.GetLength(1); columnas ++)
                         {
-                        var chares = "MALA";
+                        var chares = "RBGA";
                         var stringChares = new char[1];
                         var random = new Random(); //Metodo invocable random
                         for (int i = 0; i < stringChares.Length; i++)
@@ -41,6 +43,34 @@ namespace TideUpPhotografies
                         bidimensionalArray[filas, columnas] = throwString;
                     }
                 }
+                // Selecciona componente de la matriz 
+                Console.WriteLine("Indica el numero de fotografia a evaluar");
+                var photoSelection = Console.ReadLine();
+                int photoSelectionParced = int.Parse(photoSelection);
+
+                //Analisis de colores
+                Console.WriteLine(divider);
+                var colorValues = "";
+                //Loop para colores
+                for ( int col =0; col<bidimensionalArray.GetLength(0); col ++)
+                {
+                    Console.Write(bidimensionalArray[photoSelectionParced, col]);
+                    colorValues = colorValues + bidimensionalArray[photoSelectionParced, col];
+            }
+                //Evaluador de color
+                Console.WriteLine(divider);
+                if(!colorValues.Contains("")&&!colorValues.Contains(""))
+                {
+                    Console.WriteLine(" Blanco y Negro");
+                }
+                else if (colorValues.Contains("") || !colorValues.Contains("") || !colorValues.Contains(""))
+                {
+                    System.Console.WriteLine("Codigo de Color: ");
+                }
+            }
+            catch (IndexOutOfRangeException outOfRangeEx) //Excepcion para index  mayor al indicado
+            {
+                System.Console.WriteLine(outOfRangeEx);
             }
         }
     }
